dab7f21dc70037aed5f423eac5ad45a38f8d72ae
ecviv <ecvivar@gmail.com>
2025-10-04 20:47:10 -0300

    Menu Otras Operaciones

--- app/src/main/java/com.example.proyectointegradorfreekoders/MenuPrincipal.kt
+++ app/src/main/java/com.example.proyectointegradorfreekoders/MenuPrincipal.kt
@@ -1,22 +1,28 @@
 package com.example.proyectointegradorfreekoders
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.ImageView
-import android.content.Intent
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+
 
 class MenuPrincipal : AppCompatActivity() {
+
+    private lateinit var btnMenu: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_menu_principal)
 
-        val btnMenu = findViewById<ImageView>(R.id.btnMenu)
+        btnMenu = findViewById(R.id.btnMenu)
 
         btnMenu.setOnClickListener {
-            val intent = Intent(this, OtrasOperaciones::class.java)
-            startActivity(intent)
+            val bottomSheet = MenuBottomSheet()
+            bottomSheet.show(supportFragmentManager, "MenuBottomSheet")
         }
 
     }
