04e16c997c34536310a34e856ff847f851bfbc86
Cristina <clmbjcm@gmail.com>
2025-10-05 06:40:34 -0300

    FIX: borde y color texto menú inicio

---
+++ app/src/main/java/com/example/proyectointegradorfreekoders/PagoCorrectoActivity.kt
@@ -0,0 +1,22 @@
+package com.example.proyectointegradorfreekoders
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.button.MaterialButton
+
+class PagoCorrectoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pago_correcto)
+
+        val btnVolverInicio = findViewById<MaterialButton>(R.id.btnVolverInicio)
+        btnVolverInicio.setOnClickListener {
+            val intent = Intent(this, MenuPrincipal::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(intent)
+            finish()
+        }
+
+    }
+}
