63fe5f88c36c0f4f8c5beec57fb3232c3af2ab0c
ecviv <ecvivar@gmail.com>
2025-10-05 21:59:16 -0300

    AsignarActividadNoSocio2

---
+++ app/src/main/java/com/example/proyectointegradorfreekoders/AsignarActividadNoSocio2.kt
@@ -0,0 +1,21 @@
+package com.example.proyectointegradorfreekoders
+
+import android.R.*
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.SearchView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class AsignarActividadNoSocio2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+
+
+    }
+}
