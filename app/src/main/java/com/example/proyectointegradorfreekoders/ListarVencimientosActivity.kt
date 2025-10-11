31e1cfee2a90f9c10ed00dcae07fd3e4062fbafb
Cristina <clmbjcm@gmail.com>
2025-10-11 10:38:59 -0300

    FIX: cambio estilos y colores pantalla Vencimientos

--- app/src/main/java/com/example/proyectointegradorfreekoders/ListarVencimientosActivity.kt
+++ app/src/main/java/com/example/proyectointegradorfreekoders/ListarVencimientosActivity.kt
@@ -1,20 +1,27 @@
 package com.example.proyectointegradorfreekoders
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageView
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.button.MaterialButton
 
 class ListarVencimientosActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Carga el layout que hemos diseñado
         setContentView(R.layout.activity_listarvencimientos)
 
-        val flechahaciaAtras = findViewById<ImageView>(R.id.iv_back)
-        flechahaciaAtras.setOnClickListener {
+        // botón volver
+        val botonVolver = findViewById<MaterialButton>(R.id.btnVolver) // Corregido el ID
+        botonVolver.setOnClickListener {
             finish()
         }
 
+        //  botón IMPRIMIR PDF
+        val botonImprimir = findViewById<Button>(R.id.imprimir)
+        botonImprimir.setOnClickListener {
+            Toast.makeText(this, "Imprimiendo la lista de vencimientos", Toast.LENGTH_SHORT).show()
+        }
     }
 }
